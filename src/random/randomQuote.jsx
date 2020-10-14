30741172d1753dd461af1be19c75f40bdbe1e102
Simon Johnson <simon@hpsad.com>
2020-10-14 10:39:29 +0530

    random quote generated

--- src/random/randomQuote.jsx
+++ src/random/randomQuote.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { Component, useEffect, useState } from 'react'
+import QuoteCard from '../common-component/quote_card'
+import Axios from 'axios'
 
-function randomQuote(props){
+function RandomQuote(props){
+
+    const [quote,setQuote]=useState([])
+
+    useEffect(()=>{
+       Axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+       .then(response =>
+        {
+            setQuote(response.data)
+          
+        }
+           
+       ) 
+       .catch((err) => console.log(err))
+    },[])
+
+  const  handleClick=()=>{
+        Axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+        .then(response =>
+         {
+             setQuote(response.data)
+           
+         }
+            
+        ) 
+        .catch((err) => console.log(err))
+ 
+    }
+
+//     const  handleClick=()=>{
+        
+//    }
     return(
+       
         <div>
-            handleClick=()=>{
-                 Axios.get('https://jsonplaceholder.typicode.com/users')
-                 .then(response =>
-                     response.data
-                 ) 
-                 .catch((err) => console.log(err))
+
+            {quote.length>0 &&
+           <QuoteCard quote={quote[0].quote } author={quote[0].character}></QuoteCard>
             }
+            <button onClick={handleClick}>Get Another Quote</button>
         </div>
     )
 }
 
 
-export default randomQuote
+export default RandomQuote
